a4dde81a910627ad5e70c3214ddcecfe2fb2ad00
JWileczek <john.wileczek@prominentedge.com>
2020-12-10 11:21:01 -0500

    Adding migration to user table that will update any old 'Contractor' affiliation users to the new 'Other' service affiliation.

---
+++ server/migration/1607616094276-ContractorAffiliationUpdate.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class ContractorAffiliationUpdate1607616094276 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`UPDATE "user" SET "service"='Other' WHERE "service"='Contractor'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.query(`UPDATE "user" SET "service"='Contractor' WHERE "service"='Other'`);
+    }
+
+}
