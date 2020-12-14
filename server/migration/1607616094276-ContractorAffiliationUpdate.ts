7c8649cfc2e1a5786314e7c700ed76d315e350e5
JWileczek <john.wileczek@prominentedge.com>
2020-12-14 13:23:20 -0500

    fix spacing for eslint

--- server/migration/1607616094276-ContractorAffiliationUpdate.ts
+++ server/migration/1607616094276-ContractorAffiliationUpdate.ts
@@ -1,12 +1,12 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class ContractorAffiliationUpdate1607616094276 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE "user" SET "service"='Other' WHERE "service"='Contractor'`);
+    await queryRunner.query(`UPDATE "user" SET "service"='Other' WHERE "service" = 'Contractor'`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`UPDATE "user" SET "service"='Contractor' WHERE "service"='Other'`);
+    await queryRunner.query(`UPDATE "user" SET "service"='Contractor' WHERE "service" = 'Other'`);
   }
 }
