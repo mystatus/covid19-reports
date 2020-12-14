012a8f4923d7b4b0950dfb9b16fcce2e156cd5ca
JWileczek <john.wileczek@prominentedge.com>
2020-12-14 10:33:16 -0500

    Fix indentation issues for eslint

--- server/migration/1607616094276-ContractorAffiliationUpdate.ts
+++ server/migration/1607616094276-ContractorAffiliationUpdate.ts
@@ -2,12 +2,11 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 
 export class ContractorAffiliationUpdate1607616094276 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`UPDATE "user" SET "service"='Other' WHERE "service"='Contractor'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`UPDATE "user" SET "service"='Other' WHERE "service"='Contractor'`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-      await queryRunner.query(`UPDATE "user" SET "service"='Contractor' WHERE "service"='Other'`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`UPDATE "user" SET "service"='Contractor' WHERE "service"='Other'`);
+  }
 }
