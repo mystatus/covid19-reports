2903b6c9455c5cb9f46f7d614b8924a23487c8bf
JWileczek <john.wileczek@prominentedge.com>
2020-12-14 15:29:58 -0500

    fix double import quotes for eslint

--- server/migration/1607616094276-ContractorAffiliationUpdate.ts
+++ server/migration/1607616094276-ContractorAffiliationUpdate.ts
@@ -1,4 +1,4 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class ContractorAffiliationUpdate1607616094276 implements MigrationInterface {
 
