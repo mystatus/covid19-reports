99bf3204b578f494c1e917b5834d18d8184de095
Anthony Whitaker <awhitaker@gmail.com>
2021-02-09 10:24:56 -0500

    Validation for registration page

--- src/components/pages/user-registration-page/user-registration-page.tsx
+++ src/components/pages/user-registration-page/user-registration-page.tsx
@@ -4,6 +4,7 @@ import {
 import {
   AssignmentOutlined, DoneAll, FavoriteBorder, LocalHospital, Timeline,
 } from '@material-ui/icons';
+import MuiPhoneNumber from 'material-ui-phone-number';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { User, UserRegisterData } from '../../../actions/user.actions';
@@ -13,29 +14,89 @@ import useStyles from './user-registration-page.styles';
 import services from '../../../data/services';
 import { ButtonWithSpinner } from '../../buttons/button-with-spinner';
 
+// from: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+function validateEmail(email: string) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validatePhone(phone: string) {
+  const numeric = phone.replace(/\D/g, '');
+  return numeric.length === 10;
+}
+
+type UserRegisterDataWithValidation = {
+  [key in keyof UserRegisterData]: {
+    hasBlurred: boolean;
+    hasChanged: boolean;
+    helperText: string;
+    value: string;
+  };
+};
+
 export const UserRegistrationPage = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const user = useSelector<AppState, UserState>(state => state.user);
   const [registerUserLoading, setRegisterUserLoading] = useState(false);
-  const [inputData, setInputData] = useState<UserRegisterData>({
-    firstName: '',
-    lastName: '',
-    phone: '',
-    service: '',
-    email: '',
+  const [inputData, setInputData] = useState<UserRegisterDataWithValidation>({
+    firstName: { hasBlurred: false, hasChanged: false, helperText: '', value: '' },
+    lastName: { hasBlurred: false, hasChanged: false, helperText: '', value: '' },
+    phone: { hasBlurred: false, hasChanged: false, helperText: '', value: '' },
+    service: { hasBlurred: false, hasChanged: false, helperText: '', value: '' },
+    email: { hasBlurred: false, hasChanged: false, helperText: '', value: '' },
   });
 
-  function handleInputChange(key: keyof UserRegisterData, e: React.ChangeEvent<{ value: unknown }>) {
+  function getInputValidation(key: keyof UserRegisterData) {
+    const { hasBlurred, hasChanged, helperText } = inputData[key] ?? {};
+    if (helperText && hasBlurred && hasChanged) {
+      return {
+        error: true,
+        helperText,
+      };
+    }
+    return null;
+  }
+
+  function handleValidation(key: keyof UserRegisterData, value: string, fromBlur = true) {
+    let helperText = '';
+    if (!value) {
+      helperText = 'Required';
+    } else if (key === 'email' && !validateEmail(value)) {
+      helperText = 'Invalid email address';
+    } else if (key === 'phone' && !validatePhone(value)) {
+      helperText = 'Invalid phone number';
+    }
+    inputData[key] = {
+      hasBlurred: inputData[key].hasBlurred || fromBlur,
+      hasChanged: inputData[key].hasChanged || !fromBlur,
+      helperText,
+      value,
+    };
+    setInputData({ ...inputData });
+  }
+
+  function handleChange(key: keyof UserRegisterData, value: string) {
     setInputData({
       ...inputData,
-      [key]: e.target.value as string,
+      [key]: value,
     });
+    handleValidation(key, value, false);
+  }
+
+  function handleInputChange(key: keyof UserRegisterData, e: React.ChangeEvent<{ value: unknown }>) {
+    handleChange(key, e.target.value as string);
   }
 
   async function handleCreateAccountClick() {
     setRegisterUserLoading(true);
-    await dispatch(User.register(inputData));
+    await dispatch(User.register({
+      firstName: inputData.firstName.value,
+      lastName: inputData.lastName.value,
+      phone: inputData.phone.value,
+      service: inputData.service.value,
+      email: inputData.email.value,
+    }));
     setRegisterUserLoading(false);
   }
 
@@ -44,8 +105,10 @@ export const UserRegistrationPage = () => {
       !inputData.firstName
       || !inputData.lastName
       || !inputData.phone
+      || !validatePhone(inputData.phone.value)
       || !inputData.service
       || !inputData.email
+      || !validateEmail(inputData.email.value)
     );
   }
 
@@ -92,13 +155,17 @@ export const UserRegistrationPage = () => {
                 id="firstName"
                 label="First name"
                 required
+                onBlur={e => handleValidation('firstName', e.target.value)}
                 onChange={e => handleInputChange('firstName', e)}
+                {...getInputValidation('firstName')}
               />
               <TextField
                 id="lastName"
                 label="Last name"
                 required
+                onBlur={e => handleValidation('lastName', e.target.value)}
                 onChange={e => handleInputChange('lastName', e)}
+                {...getInputValidation('lastName')}
               />
             </div>
             <div>
@@ -109,21 +176,35 @@ export const UserRegistrationPage = () => {
                 required
                 disabled
               />
-              <TextField
+              <MuiPhoneNumber
+                defaultCountry="us"
+                disableCountryCode
+                disableDropdown
+                id="phone"
+                label="Phone number"
+                onBlur={(e: any) => handleValidation('phone', e.nativeEvent.target.value)}
+                onChange={(value: string) => handleChange('phone', value)}
+                placeholder="(702) 123-4567"
+                required
+                {...getInputValidation('phone')}
+              />
+              {/* <TextField
                 id="phone"
                 label="Phone number"
                 required
                 onChange={e => handleInputChange('phone', e)}
-              />
+              /> */}
             </div>
             <div>
               <TextField
                 id="service"
                 label="Service"
                 select
-                value={inputData.service}
+                value={inputData.service.value}
                 required
+                onBlur={e => handleValidation('service', e.target.value)}
                 onChange={e => handleInputChange('service', e)}
+                {...getInputValidation('service')}
               >
                 {services.map(serviceName => (
                   <MenuItem key={serviceName} value={serviceName}>
@@ -136,7 +217,9 @@ export const UserRegistrationPage = () => {
                 label="Email"
                 type="email"
                 required
+                onBlur={e => handleValidation('email', e.target.value)}
                 onChange={e => handleInputChange('email', e)}
+                {...getInputValidation('email')}
               />
             </div>
           </form>
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,4 @@
 /* eslint-disable */
 
 declare module 'react-charts';
+declare module 'material-ui-phone-number';
