a296fa09cd64997305c63d826ecfabf6342caac9
Anthony Whitaker <awhitaker@gmail.com>
2021-03-17 17:17:24 -0400

    Simplified Oprhaned Records UX

--- src/components/pages/roster-page/roster-page.tsx
+++ src/components/pages/roster-page/roster-page.tsx
@@ -418,50 +418,9 @@ export const RosterPage = () => {
     });
   };
 
-  const assignOrphanToSelfClicked = async (row: ApiOrphanedRecord) => {
-    const result = await dispatch(Modal.alert('Claim Orphaned Record', '<span>This orphaned record will be temporarily assigned to you and will be hidden from other roster managers while you attempt to resolve the situation.</span><p>How long would you like to claim the record?</p>', [
-      { text: 'Permanently', value: -1, className: classes.deleteOrphanedRecordConfirmButton },
-      { text: '4 hours', value: 4 },
-      { text: '1 day', value: 24 },
-      { text: '1 week', value: 7 * 24 },
-      { text: 'Cancel', variant: 'outlined' },
-    ]));
-    if (!result?.button?.value) {
-      return;
-    }
-    try {
-      setOrphanedRecordsWaiting(true);
-      await axios.put(`api/orphaned-record/${orgId}/${row.id}/action`, {
-        action: 'claim',
-        timeToLiveMs: Math.max(0, result.button.value * 60 * 60 * 1000),
-      });
-
-      await initializeTable();
-    } catch (error) {
-      dispatch(Modal.alert('Assign Orphan', formatMessage(error, 'Unable to assign claim the record')));
-    } finally {
-      setOrphanedRecordsWaiting(false);
-    }
-  };
-
-  const unclaimOrphanedRecord = async (row: ApiOrphanedRecord) => {
-    try {
-      setOrphanedRecordsWaiting(true);
-      await axios.delete(`api/orphaned-record/${orgId}/${row.id}/claim`);
-      await initializeTable();
-    } catch (error) {
-      dispatch(Modal.alert('Assign Orphan', formatMessage(error, 'Unable to assign claim the record')));
-    } finally {
-      setOrphanedRecordsWaiting(false);
-    }
-  };
-
   const ignoreOrphanClicked = async (row: ApiOrphanedRecord) => {
-    const result = await dispatch(Modal.alert('Ignore Orphaned Record', '<span>This orphan record will be temporarily hidden from your view, but will still be visible to other roster managers.</span><p>How long should we ignore the record?</p>', [
-      { text: 'Permanently', value: -1, className: classes.deleteOrphanedRecordConfirmButton },
-      { text: '4 hours', value: 4 },
-      { text: '1 day', value: 24 },
-      { text: '1 week', value: 7 * 24 },
+    const result = await dispatch(Modal.alert('Ignore Orphaned Record', 'This orphaned record will be permanently hidden from your view but will remain visible to other roster managers.', [
+      { text: 'Ignore', value: -1, className: classes.deleteOrphanedRecordConfirmButton },
       { text: 'Cancel', variant: 'outlined' },
     ]));
     if (!result?.button?.value) {
@@ -487,24 +446,8 @@ export const RosterPage = () => {
       if (column.name === 'earliestReportDate' || column.name === 'latestReportDate') {
         return moment(value).format('Y-M-D h:mm A z');
       }
-      if (column.name === 'claimedUntil') {
-        return moment().utc().to(moment(value), true);
-      }
-    }
-
-    if (!value && orphanedRecord.action === 'claim') {
-      return 'Permanent';
     }
     return value;
-  };
-
-  const getOrphanRowProps = (orphanedRecord: ApiOrphanedRecord) => {
-    if (orphanedRecord.action === 'claim') {
-      return {
-        className: classes.claimedOrphanRow,
-      };
-    }
-    return null;
   };
 
   //
@@ -533,7 +476,6 @@ export const RosterPage = () => {
                 { name: 'count', displayName: 'Count' },
                 { name: 'earliestReportDate', displayName: 'First Report' },
                 { name: 'latestReportDate', displayName: 'Latest Report' },
-                { name: 'claimedUntil', displayName: 'Remaining Claim' },
               ]}
               title={`Orphaned Records (${orphanedRecords.length})`}
               idColumn="id"
@@ -543,23 +485,11 @@ export const RosterPage = () => {
                   disabled: orphanedRecordsWaiting,
                   name: 'Add to a Roster...',
                 }, {
-                  callback: assignOrphanToSelfClicked,
-                  disabled: orphanedRecordsWaiting,
-                  hidden: row.action === 'claim',
-                  name: 'Claim...',
-                }, {
-                  callback: unclaimOrphanedRecord,
-                  disabled: orphanedRecordsWaiting,
-                  hidden: row.action !== 'claim',
-                  name: 'Unclaim',
-                }, {
                   callback: ignoreOrphanClicked,
                   disabled: orphanedRecordsWaiting,
-                  hidden: row.action === 'claim',
                   name: 'Ignore...',
                 }]),
                 renderCell: getOrphanCellDisplayValue,
-                rowProps: getOrphanRowProps,
               }}
             />
           </TableContainer>
