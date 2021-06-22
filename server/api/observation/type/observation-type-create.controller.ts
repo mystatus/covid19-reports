df815aeeff7f4017a39fa6f34d5232db736ed1a6
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-22 10:44:48 -0500

    add Observation Type entity

--- server/api/observation/type/observation-type-create.controller.ts
+++ server/api/observation/type/observation-type-create.controller.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express';
 import { ObservationType } from './observation-type.model';
 
-function toObservationType(type: string) {
-  const observationType = new ObservationType();
-  observationType.type = type;
-  return observationType;
-}
-
 class ObservationTypeCreateController {
   async createObservationType(req: Request, res: Response) {
     return res.json(await ObservationType.save(toObservationType(req.params.type)));
   }
+}
+
+function toObservationType(type: string) {
+  const observationType = new ObservationType();
+  observationType.type = type;
+  return observationType;
 }
 
 
