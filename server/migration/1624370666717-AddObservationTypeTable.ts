3ecf6630a620e3946db55d626162a363910c4c79
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-22 14:32:23 -0500

    add Observation Type entity

--- server/migration/1624370666717-AddObservationTypeTable.ts
+++ server/migration/1624370666717-AddObservationTypeTable.ts
@@ -5,11 +5,9 @@ export class AddObservationTypeTable1624370666717 implements MigrationInterface 
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`CREATE TABLE "observation_type" ("type" character varying(100) NOT NULL, CONSTRAINT "UQ_45daf0e906ab42b60abeaf83bd5" UNIQUE ("type"), CONSTRAINT "PK_45daf0e906ab42b60abeaf83bd5" PRIMARY KEY ("type"))`);
-    await queryRunner.query(`ALTER TABLE "user_notification_setting" ALTER COLUMN "last_notified_date" SET DEFAULT null`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "user_notification_setting" ALTER COLUMN "last_notified_date" DROP DEFAULT`);
     await queryRunner.query(`DROP TABLE "observation_type"`);
   }
 
