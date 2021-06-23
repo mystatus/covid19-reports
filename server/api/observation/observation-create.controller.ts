db40122b8535abc531944f2008d3f37f898df290
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-23 12:58:53 -0500

    add Observation entity

--- server/api/observation/observation-create.controller.ts
+++ server/api/observation/observation-create.controller.ts
@@ -4,21 +4,21 @@ import { ObservationType } from './type/observation-type.model';
 
 class ObservationCreateController {
   async createObservation(req: Request, res: Response) {
-
-    console.log(JSON.stringify(req.body));
-
-    const observation = new Observation();
-    observation.documentId = req.body.documentId;
-    observation.edipi = req.body.edipi;
-    observation.timestamp = req.body.timestamp;
-    observation.unitId = req.body.unitId;
-    observation.unit = req.body.unit;
-    observation.orgId = req.body.orgId;
-    observation.type = new ObservationType();
-    observation.type.type = req.body.type;
-
-    return res.json(await observation.save());
+    return res.json(await toObservation(req).save());
   }
+}
+
+function toObservation(req: Request) {
+  const observation = new Observation();
+  observation.documentId = req.body.documentId;
+  observation.edipi = req.body.edipi;
+  observation.timestamp = req.body.timestamp;
+  observation.unitId = req.body.unitId;
+  observation.unit = req.body.unit;
+  observation.orgId = req.body.orgId;
+  observation.type = new ObservationType();
+  observation.type.type = req.body.type;
+  return observation;
 }
 
 
