0b8d591a28b6fed7bcce838aaa9fafde6ba66e38
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-24 09:21:33 -0500

    remove observation type

--- server/api/observation/type/observation-type.model.ts
+++
@@ -1,9 +0,0 @@
-import { BaseEntity, Entity, PrimaryColumn, Unique } from 'typeorm';
-
-@Entity()
-@Unique(['type'])
-export class ObservationType extends BaseEntity {
-
-  @PrimaryColumn({ length: 100 })
-  type: string;
-}
