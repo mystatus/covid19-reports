391f7b237f9f876b32bb34f8836f9caddc49a349
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-25 11:04:16 -0500

    Replace ObservationType with ReportSchema

--- server/api/observation/observation-router.ts
+++ server/api/observation/observation-router.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import getObservationController from './observation-get.controller';
 import createObservationController from './observation-create.controller';
+import { requireInternalUser } from '../../auth';
 
 
 const observationRouter = express.Router() as any;
 
-observationRouter.get('/', getObservationController.getAllObservations);
-observationRouter.post('/', express.json(), createObservationController.createObservation);
+observationRouter.get('/', requireInternalUser, getObservationController.getAllObservations);
+observationRouter.post('/', requireInternalUser, express.json(), createObservationController.createObservation);
 
 
 export default observationRouter;
