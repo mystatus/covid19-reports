08e069c43190355b292089dfea748d6a5074e836
Peter Frollo <peter.frollo@prominentedge.com>
2021-06-25 10:42:56 -0500

    Replace ObservationType with ReportSchema

--- server/api/observation/observation-create.controller.ts
+++ server/api/observation/observation-create.controller.ts
@@ -1,48 +1,20 @@
+import { getManager } from 'typeorm';
 import { Request, Response } from 'express';
-import { Observation } from './observation.model';
-import { ReportSchema } from '../report-schema/report-schema.model';
-import { NotFoundError } from '../../util/error-types';
-import { Org } from '../org/org.model';
 
 class ObservationCreateController {
   async createObservation(req: Request, res: Response) {
-    return res.json(await (toObservation(req, await toType(req, await toOrg(req))).save()));
+    return res.json(await createObservation(req));
   }
 }
 
-async function toOrg(req: Request) {
-  const id = req.body.type_org;
-  const org = await Org.findOne(id);
-
-  if (!org) {
-    throw new NotFoundError(`Org not found for id ${id}`);
-  }
-
-  return org;
-}
-
-async function toType(req: Request, org: Org) {
-  const id = req.body.type_id;
-  const type = await ReportSchema.findOne({ where: { id, org } });
-
-  if (!type) {
-    throw new NotFoundError(`ReportSchema not found for id ${id} and org_id ${org.id}`);
-  }
-
-  return type;
-}
-
-function toObservation(req: Request, type: ReportSchema) {
-
-  const observation = new Observation();
-  observation.documentId = req.body.documentId;
-  observation.edipi = req.body.edipi;
-  observation.timestamp = req.body.timestamp;
-  observation.unitId = req.body.unitId;
-  observation.unit = req.body.unit;
-  observation.type = type;
-
-  return observation;
+async function createObservation(req: Request) {
+  // getManager().query() does not return the inserted result
+  await getManager()
+    .query(
+      'INSERT INTO public.observation(document_id, edipi, timestamp, unit_id, unit, type_id, type_org)'
+      + 'VALUES ($1, $2, $3, $4, $5, $6, $7)',
+      [req.body.documentId, req.body.edipi, req.body.timestamp, req.body.unitId, req.body.unit, req.body.type_id, req.body.type_org],
+    );
 }
 
 
