bc7e8afef3d2ce23c4f8a727a9d4bfb002985227
Peter Frollo <peter.frollo@prominentedge.com>
2021-07-15 09:51:06 -0500

    Add phone# to Roster

--- server/src/api/roster/roster-utils.ts
+++ server/src/api/roster/roster-utils.ts
@@ -1,11 +1,16 @@
 import { Roster } from './roster.model';
 import { BadRequestError } from '../../util/error-types';
 
-export async function saveRosterPhoneNumber(edipi: string, phoneNumber: string) {
-  const roster = await Roster.findOne({ where: { edipi }});
-  if (roster) {
-    roster.phoneNumber = phoneNumber;
-    return roster.save();
+export async function saveRosterPhoneNumber(edipi: string, phoneNumber: string): Promise<void> {
+  // The same edipi can potentially be in the roster table multiple times
+  // since an individual can be on multiple units.
+  const rosters: Roster[] = await Roster.find({ where: { edipi }});
+  if (!rosters || rosters.length === 0) {
+    throw new BadRequestError(`Unable to find ${edipi} in Roster`);
   }
-  throw new BadRequestError(`Unable to find ${edipi} in Roster`);
+  rosters.forEach(async roster => {
+    roster.phoneNumber = phoneNumber;
+    await roster.save();
+  });
+  return Promise.resolve();
 }
