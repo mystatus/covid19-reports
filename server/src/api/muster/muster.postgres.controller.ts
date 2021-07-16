3e92208130693aff7218ef02ac85fb6a36b22a78
Peter Frollo <peter.frollo@prominentedge.com>
2021-07-16 09:32:11 -0500

    add muster compliance calculations

--- server/src/api/muster/muster.postgres.controller.ts
+++ server/src/api/muster/muster.postgres.controller.ts
@@ -6,12 +6,13 @@ import { ApiRequest, OrgRoleParams, Paginated} from '../api.router';
 import { Roster } from '../roster/roster.model';
 import { GetMusterRosterQuery } from './muster.controller';
 import { Unit, MusterConfiguration } from '../unit/unit.model';
+import { Org } from '../org/org.model';
 
 
 class MusterPostgresController {
 
   /**
-   * Provides Muster Compliance details on the individual bases.
+   * Provides Muster Compliance details for all service members
    */
   async getMusterRoster(req: ApiRequest<OrgRoleParams, null, GetMusterRosterQuery>, res: Response<Paginated<Partial<MusterComplianceReport>>>) {
     assertRequestQuery(req, [
@@ -29,29 +30,20 @@ class MusterPostgresController {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const userRole = req.appUserRole!;
 
-    const limit = parseInt(req.query.limit);
-    const page = parseInt(req.query.page);
+    const rowLimit = parseInt(req.query.limit);
+    const pageNumber = parseInt(req.query.page);
     const unitId = req.query.unitId!;
     const orgId = req.appOrg!.id;
 
 
-    const rosters = await this.getRosters(unitId, orgId);
-    const unitsInRosters = rosters.map(r => r.unit.id);
-    const unitsMusterConf = await this.getUnitsMusterConf(unitsInRosters);
-    const musterInfo: MusterComplianceReport[] = rosters
-      .map(roster => {
-        return {
-          totalMusters: 1,
-          mustersReported: 0,
-          musterPercent: 0,
-          edipi: roster.edipi,
-          firstName: roster.firstName,
-          lastName: roster.lastName,
-          myCustomColumn1: 'tbd',
-          unitId: roster.unit.id,
-          phone: roster.phoneNumber,
-        };
-      });
+    const filteredRosters: Roster[] = await MusterPostgresController.getRosters(unitId, orgId);
+    const unitsInRosters: number[] = MusterPostgresController.getUnitIds(filteredRosters);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const unitsMusterConf: MusterUnitConfiguration[] = await MusterPostgresController.setDefaultMusterConf(
+      await MusterPostgresController.getMusterUnitConf(unitsInRosters), orgId,
+    );
+
+    const intermediateMusterInfo: MusterComplianceReport[] = filteredRosters.map(roster => MusterPostgresController.toMasterInfo(roster));
 
     /*
       The muster page needs:
@@ -66,10 +58,18 @@ class MusterPostgresController {
           - do the % compliance calculation
      */
 
-    return res.json({ rows: this.toPageWithRowLimit(musterInfo, page, limit), totalRowsCount: musterInfo.length });
+    return res.json({
+      rows: MusterPostgresController.toPageWithRowLimit(intermediateMusterInfo, pageNumber, rowLimit),
+      totalRowsCount: intermediateMusterInfo.length,
+    });
   }
 
-  private async getRosters(unitId: number, orgId: number) {
+  /** Get an array of unique Unit IDs */
+  private static getUnitIds(rosters: Roster[]) {
+    return Array.from(new Set(rosters.map(r => r.unit.id)));
+  }
+
+  private static async getRosters(unitId: number, orgId: number) {
     let rosters;
     if (unitId) {
       rosters = await Roster.find({ relations: ['unit', 'unit.org'], where: { unit: unitId } });
@@ -79,29 +79,54 @@ class MusterPostgresController {
     return rosters.filter(roster => roster.unit.org!.id === orgId);
   }
 
-  private async getUnitsMusterConf(unitIds: number[]) {
-
-    const unitsConf = await Unit.find({where: { unit: In(unitIds)}});
-    const musterUnitConf = unitsConf.map(unitConf => {
+  private static async getMusterUnitConf(unitIds: number[]) {
+    return (await Unit.find({ where: { id: In(unitIds) } })).map(unitConf => {
       return {
         unitId: unitConf.id,
         musterConf: unitConf.musterConfiguration,
       };
     });
-
-    if (this.isMusterConfMissing(musterUnitConf)) {
-
-    }
-
-
   }
 
-  private isMusterConfMissing(musterUnitConf: MusterUnitConfiguration[]) {
+  /**
+   * Sets default muster configuration where muster configuration is missing.
+   * This function has a side effect, it modifies musterUnitConf.
+   */
+  private static async setDefaultMusterConf(musterUnitConf: MusterUnitConfiguration[], orgId: number): Promise<MusterUnitConfiguration[]> {
+    if (MusterPostgresController.isMusterConfMissing(musterUnitConf)) {
+      const org = await Org.findOne({ where: { id: orgId } });
+      if (org) {
+        const defaultOrgMusterConf = org.defaultMusterConfiguration;
+        musterUnitConf.forEach(muc => {
+          if (!muc.musterConf || muc.musterConf.length === 0) {
+            muc.musterConf = defaultOrgMusterConf;
+          }
+        });
+      }
+    }
+    return musterUnitConf;
+  }
+
+  private static isMusterConfMissing(musterUnitConf: MusterUnitConfiguration[]) {
     const confWithMissingMusterConf = musterUnitConf.filter(conf => !conf.musterConf || conf.musterConf.length === 0);
     return confWithMissingMusterConf.length > 0;
   }
 
-  private toPageWithRowLimit(musterInfo: MusterComplianceReport[], page: number, limit: number) {
+  private static toMasterInfo(roster: Roster) {
+    return {
+      totalMusters: 1,
+      mustersReported: 0,
+      musterPercent: 0,
+      edipi: roster.edipi,
+      firstName: roster.firstName,
+      lastName: roster.lastName,
+      myCustomColumn1: 'tbd',
+      unitId: roster.unit.id,
+      phone: roster.phoneNumber,
+    };
+  }
+
+  private static toPageWithRowLimit(musterInfo: MusterComplianceReport[], page: number, limit: number) {
     const offset = page * limit;
     return musterInfo.slice(offset, offset + limit);
   }
@@ -111,7 +136,6 @@ type MusterUnitConfiguration = {
   unitId: number
   musterConf: MusterConfiguration[]
 };
-
 
 type MusterComplianceReport = {
   totalMusters: number
