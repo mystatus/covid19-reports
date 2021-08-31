a205b0032d21ccfd2e392c66206e7128caa2f8b5
Roel Pasetes <roel.pasetes@prominentedge.com>
2021-08-31 14:19:46 -0500

    added return codes to build scripts to abort on failure (#214)

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ export NODE_OPTIONS="--max-old-space-size=4096"
 mkdir build
 
 # Server
-yarn run server build
+yarn run server build || exit 1
 
 mv ./packages/server/build/* ./build
 rmdir ./packages/server/build
 
 # Client
-yarn run client build
+yarn run client build || exit 1
 
 mv ./packages/client/build/* ./build
 rmdir ./packages/client/build
--- packages/shared/build.sh
+++ packages/shared/build.sh
@@ -5,6 +5,6 @@ export NODE_OPTIONS="--max-old-space-size=4096"
 echo -e "\nBuilding shared..."
 
 ./clean.sh
-tsc -b tsconfig.build.json
+tsc -b tsconfig.build.json || exit 1
 
 echo "Shared build complete!"
