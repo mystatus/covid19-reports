2d1376fb930c09bae9d8c6e8fb86a853d79df7cf
Anthony Whitaker <awhitaker@gmail.com>
2021-09-10 17:07:13 -0400

    Fix observations, again (#224)

--- packages/server/src/api/observation/observation.controller.ts
+++ packages/server/src/api/observation/observation.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { EntityManager, getConnection } from 'typeorm';
-import { RosterInfo } from '@covid19-reports/shared';
-import { ObservationApiModel, ObservationApiRawReportModel } from './observation.types';
+import { CustomColumns, RosterInfo } from '@covid19-reports/shared';
+import { ObservationApiModel, Report } from './observation.types';
 import { Observation } from './observation.model';
 import { ApiRequest, EdipiParam } from '../api.router';
 import { Log } from '../../util/log';
@@ -42,33 +42,32 @@ class ObservationController extends EntityController<Observation> {
     return res.json(await Observation.find());
   }
 
-  async createObservation(req: ApiRequest<EdipiParam, ObservationApiRawReportModel>, res: Response) {
+  async createObservation(req: ApiRequest<EdipiParam, Report>, res: Response) {
     Log.info('Creating observation', req.body);
 
-    assertRequestBody(req, ['EDIPI', 'Timestamp', 'ID']);
+    const { EDIPI, ReportType, Timestamp } = assertRequestBody(req, ['EDIPI', 'Timestamp', 'ID']);
+    const rosters = await getRosterInfosForIndividualOnDate(EDIPI, `${Timestamp}`);
 
-    const model = rawReportToObservationApiModel(req.body);
-    const { edipi, phoneNumber, reportSchemaId, timestamp } = model;
-
-    const rosters: RosterInfo[] = await getRosterInfosForIndividualOnDate(edipi, `${timestamp}`);
-
-    if (!rosters || rosters.length === 0) {
-      Log.error(`Unable to save observation from: ${req.body}`);
+    if (rosters?.length !== 1) {
+      Log.error(`Unable to save observation. Individual exists on ${rosters?.length} rosters. From:`, req.body);
       throw new BadRequestError('Unable to save observation');
     }
 
     const observation = await getConnection().transaction(async manager => {
-      for (const roster of rosters) {
-        if (hasReportingGroup(roster, getReportingGroup(req))) {
-          const reportSchema = await findReportSchema(reportSchemaId, roster.unit.org?.id);
-          if (reportSchema) {
-            await saveRosterPhoneNumber(edipi, phoneNumber, manager);
-            return saveObservationWithReportSchema(req, reportSchema, manager);
-          }
-        }
+      const roster = rosters[0];
+      if (!hasReportingGroup(roster, getReportingGroup(req))) {
+        Log.error('Unable to find reporting group from:', req.body);
+        throw new BadRequestError('Reporting group not found.')
       }
 
-      return undefined;
+      const reportSchema = await findReportSchema(ReportType, roster.unit.org?.id);
+      if (!reportSchema) {
+        Log.error(`Unable to find report schema ('${ReportType}') from:`, req.body);
+        throw new BadRequestError('ReportType not found.')
+      }
+
+      await saveRosterPhoneNumber(EDIPI, `${req.body.Details?.PhoneNumber ?? ''}`, manager);
+      return saveObservationWithReportSchema(req, reportSchema, manager);
     });
 
     res.json(observation);
@@ -80,39 +79,30 @@ function hasReportingGroup(rosterInfo: RosterInfo, reportingGroup: string | unde
   return reportingGroup && rosterInfo.unit.org?.reportingGroup === reportingGroup;
 }
 
-function findReportSchema(reportSchemaId: string, orgId: number | undefined) {
+function findReportSchema(reportSchemaId?: string, orgId?: number) {
   return ReportSchema.findOne({
     relations: ['org'],
     where: {
-      id: reportSchemaId,
+      id: reportSchemaId || 'es6ddssymptomobs',
       org: orgId,
     },
   });
 }
 
-function rawReportToObservationApiModel(raw: ObservationApiRawReportModel): ObservationApiModel {
-  return {
-    edipi: raw.EDIPI,
-    timestamp: raw.Timestamp,
-    documentId: raw.ID,
-    reportSchemaId: raw.ReportType || 'es6ddssymptomobs',
-    unit: raw.Details?.Unit ?? '',
-    phoneNumber: raw.Details?.PhoneNumber ?? '',
-  };
-}
-
-function getReportingGroup(req: ApiRequest<EdipiParam, ObservationApiRawReportModel>) {
+function getReportingGroup(req: ApiRequest<EdipiParam, Report>) {
   return req.body.ReportingGroup || (req.body.Client?.Application && applicationMapping[req.body.Client?.Application]);
 }
 
-function saveObservationWithReportSchema(req: ApiRequest<EdipiParam, ObservationApiRawReportModel>, reportSchema: ReportSchema, manager: EntityManager) {
+function saveObservationWithReportSchema(req: ApiRequest<EdipiParam, Report>, reportSchema: ReportSchema, manager: EntityManager) {
   const observation = new Observation();
+  const { ID, EDIPI, ReportingGroup, ReportType, Timestamp, ...customColumns } = req.body;
   observation.documentId = req.body.ID;
   observation.edipi = req.body.EDIPI;
   observation.timestamp = timestampColumnTransformer.to(req.body.Timestamp);
   observation.reportSchema = reportSchema;
-  observation.unit = req.body.Details?.Unit ?? '';
+  observation.unit = `${req.body.Details?.Unit ?? ''}`;
   observation.reportingGroup = getReportingGroup(req);
+  observation.customColumns = customColumns as unknown as CustomColumns;
   Log.info(`Saving new observation for ${observation.documentId} documentId`);
   return manager.save(observation);
 }
